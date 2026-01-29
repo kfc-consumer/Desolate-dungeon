36d612f62de2c6f39025dd7c1b8e1427a9d4c4d7
kittisaknaiyapat-prog <kittisak.naiyapat@elev.ga.lbs.se>
2026-01-29 12:35:28 +0100

    added particle system, not done yet

--- Desolate Dungeon Project/Assets/TextMesh Pro/Scripts sidescroller/DungeonController.cs
+++ Desolate Dungeon Project/Assets/TextMesh Pro/Scripts sidescroller/DungeonController.cs
@@ -1,16 +1,48 @@
+using System.Collections;
 using UnityEngine;
 
 public class DungeonController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    Vector2 StartPosition;
+    [SerializeField] Rigidbody2D rb;
+
+
+    private void Awake()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
     {
-        
+        StartPosition = transform.position;
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("TestEnemy"))
+        {
+            Die();
+        }
+    }
+
+
+    void Die()
+    {
+        StartCoroutine(Respawn(0.5f));
+    }
+
+    IEnumerator Respawn(float duration)
+    {
+        rb.simulated = false;
+        rb.linearVelocity = new Vector2(0, 0);
+        transform.localScale = new Vector3(0, 0, 0);
+        yield return new WaitForSeconds(duration);
+        transform.position = StartPosition;
+        transform.localScale = new Vector3(1, 1, 1);
+        rb.simulated = true;
+
     }
 }
