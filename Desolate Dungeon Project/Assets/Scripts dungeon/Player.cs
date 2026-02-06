e0839e5fa80cd5bb9649d9d2a7d496b39634403d
andyphilipsung <andyphilips.ung@elev.ga.lbs.se>
2026-02-06 13:42:58 +0100

    Player healthbar, player can die, damage,

--- Desolate Dungeon Project/Assets/Scripts dungeon/Player.cs
+++ Desolate Dungeon Project/Assets/Scripts dungeon/Player.cs
@@ -8,6 +8,7 @@ public class Player : MonoBehaviour
     Rigidbody2D playerRb;
 
     InputAction moveAction;
+    InputAction jumpAction;
     Vector2 moveInput;
 
     [SerializeField] float moveSpeed;
@@ -17,17 +18,24 @@ public class Player : MonoBehaviour
     {
         playerRb = GetComponent<Rigidbody2D>();
         moveAction = InputSystem.actions.FindAction("Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
     }
 
 
     void Update()
     {
         moveInput = moveAction.ReadValue<Vector2>();
+
+        if (jumpAction.WasPerformedThisFrame())
+        {
+            playerRb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        }
+
     }
 
     void FixedUpdate()
     {
-        playerRb.linearVelocityX = moveInput.x;
+        playerRb.linearVelocityX = moveInput.x * moveSpeed;
     }
 
 }
