04495dae414e82a2e263637cffc533f128b94901
kittisaknaiyapat-prog <kittisak.naiyapat@elev.ga.lbs.se>
2026-02-16 14:37:07 +0100

    Connected TestEnemy to Weapon Script

--- Desolate Dungeon Project/Assets/TextMesh Pro/Scripts sidescroller/PlayerWeaponScript.cs
+++ Desolate Dungeon Project/Assets/TextMesh Pro/Scripts sidescroller/PlayerWeaponScript.cs
@@ -1,16 +1,47 @@
 using UnityEngine;
 
+
 public class PlayerWeaponScript : MonoBehaviour
 {
+    private float AttackCdTime;
+    public float StartAttackCdTime;
+    public Transform attackPos;
+    public float AttackRange;
+    public LayerMask whatIsEnemies;
+    public int Damage;
+    TestEnemy Enemy;
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+       Enemy = FindAnyObjectByType<TestEnemy>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (AttackCdTime <= 0)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Collider2D[] EnemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, AttackRange, whatIsEnemies);
+                for (int i = 0; i < EnemiesToDamage.Length; i++)
+                    {
+                    EnemiesToDamage[i].GetComponent<TestEnemy>().TakeDamage(Damage);
+                }
+
+
+
+            }
+            AttackCdTime = StartAttackCdTime;
+
+
+        }
+        else
+        {
+            AttackCdTime -= Time.deltaTime;
+        }
     }
 }
