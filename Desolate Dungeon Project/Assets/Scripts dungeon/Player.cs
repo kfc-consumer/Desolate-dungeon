cd300c7b846fbbcd0fde8899b4c3c2eebf29a1a2
andyphilipsung <andyphilips.ung@elev.ga.lbs.se>
2026-02-23 12:43:23 +0100

    guycvjy

--- Desolate Dungeon Project/Assets/Scripts dungeon/Player.cs
+++ Desolate Dungeon Project/Assets/Scripts dungeon/Player.cs
@@ -6,28 +6,34 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    public GameObject projectilePrefab; //
+    public Transform launchPoint; //
 
+    public float shootTime;     // cooldown between projectiles    //
+    public float shootCounter;  // cooldown timer                  //
 
     Rigidbody2D playerRb;
 
     InputAction moveAction;
     InputAction jumpAction;
-  
+    InputAction throwAction; //
+
     Vector2 moveInput;
+ 
 
     [SerializeField] float moveSpeed;
     [SerializeField] float jumpForce;
-    
-    
 
+    [SerializeField] Vector2 throwForce;  //
 
 
     void Start()
     {
         playerRb = GetComponent<Rigidbody2D>();
         moveAction = InputSystem.actions.FindAction("Move");
-        jumpAction = InputSystem.actions.FindAction("Jump");
-        
+        jumpAction = InputSystem.actions.FindAction("Jump");    
+        throwAction = InputSystem.actions.FindAction("Throw");                   //
+        shootCounter = shootTime; // starts countdown when you start the game    //
 
     }
 
@@ -41,6 +47,10 @@ public class Player : MonoBehaviour
             playerRb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
         }
 
+        if (throwAction.WasPerformedThisFrame())                //
+        {
+            playerRb.AddForce(transform.up * throwForce);       //
+        }
         
 
     }
